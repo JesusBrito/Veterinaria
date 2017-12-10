e1b265c96b74bca4224c4e7a3b15f5d0f0592cbb
JesusBrito <jesus291196@gmail.com>
2017-12-10 15:17:20 -0600

    ListView de los 4 modulos ya están finalizados, emprezaré a adaptar el detalle de cliente para poder hacer modificaciones

--- java/com/example/jesus/veterinaria/registro_medico.java
+++ java/com/example/jesus/veterinaria/registro_medico.java
@@ -1,21 +1,37 @@
 package com.example.jesus.veterinaria;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class registro_medico extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
+public class registro_medico extends AppCompatActivity implements Response.Listener<JSONArray>, Response.ErrorListener{
     Button btnGuardar, btnCancelar;
     ImageButton btnRegresar, btnHome;
+    EditText txtNombre, txtApellidos, txtRfcMedico, txtDireccion, txtTelefono, txtEmail;
     Intent cargar;
+    ProgressDialog progreso;
+    RequestQueue request;
+    JsonArrayRequest JsonArrayRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_medico);
-
+        request= Volley.newRequestQueue(this);
         inicializaComponentes();
 
         btnRegresar.setOnClickListener(
@@ -54,8 +70,27 @@ public class registro_medico extends AppCompatActivity {
                 }
         );
     }
-
     private void cargarWebService() {
+        if(txtRfcMedico.getText().toString().equals("")||
+                txtNombre.getText().toString().equals("")||
+                txtDireccion.getText().toString().equals("")||
+                txtTelefono.getText().toString().equals("")||
+                txtEmail.getText().toString().equals("")||
+                txtApellidos.getText().toString().equals("")){
+            Toast.makeText(this,"Todos los campos deben de estan correctamente llenados",Toast.LENGTH_SHORT);
+        }else{
+            progreso= new ProgressDialog(this);
+            progreso.setMessage("Cargando...");
+            progreso.show();
+
+            String url ="https://veterinary-clinic-ws.herokuapp.com/medicos/create/"+txtRfcMedico.getText().toString()+
+                    "/"+txtNombre.getText().toString()+" "+txtApellidos.getText().toString()+"/"+
+                    txtDireccion.getText().toString()+"/"+txtTelefono.getText().toString()+"/"+txtEmail.getText().toString()+"/";
+
+            url= url.replace(" ", "%20");
+            JsonArrayRequest= new JsonArrayRequest(Request.Method.GET,url,null,this,this);
+            request.add(JsonArrayRequest);
+        }
     }
 
     private void inicializaComponentes() {
@@ -63,6 +98,32 @@ public class registro_medico extends AppCompatActivity {
         btnHome=(ImageButton) findViewById(R.id.activityRegistroMedicoBtnHome);
         btnGuardar=(Button) findViewById(R.id.activity_registro_medico_btnGuardar);
         btnCancelar=(Button) findViewById(R.id.activity_registro_medico_btnCancelar);
+        txtDireccion=(EditText) findViewById(R.id.activity_registro_medico_txtDirec);
+        txtRfcMedico=(EditText) findViewById(R.id.activity_registro_medico_txtRFC);
+        txtApellidos=(EditText) findViewById(R.id.activity_registro_medico_txtApe);
+        txtTelefono=(EditText) findViewById(R.id.activity_registro_medico_txtTel);
+        txtEmail=(EditText) findViewById(R.id.activity_registro_medico_txtEmail);
+        txtNombre=(EditText) findViewById(R.id.activity_registro_medico_txtNom);
+    }
+
+    //======Métodos de Volley
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        progreso.hide();
+        Toast.makeText(this,"No se pudo registrar"+error.toString(),Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onResponse(JSONArray response) {
+        progreso.hide();
+        Toast.makeText(this,"Registro agregado exitosamente",Toast.LENGTH_SHORT).show();
+        txtNombre.setText("");
+        txtEmail.setText("");
+        txtTelefono.setText("");
+        txtApellidos.setText("");
+        txtDireccion.setText("");
+        txtRfcMedico.setText("");
     }
     private void cargaHome() {
         cargar = new Intent(this, Principal.class);
--- java/com/example/jesus/veterinaria/registro_servicio.java
+++ java/com/example/jesus/veterinaria/registro_servicio.java
@@ -1,23 +1,39 @@
 package com.example.jesus.veterinaria;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class registro_servicio extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
+public class registro_servicio extends AppCompatActivity implements Response.Listener<JSONArray>, Response.ErrorListener {
     Button btnGuardar, btnCancelar;
     ImageButton btnRegresar, btnHome;
     Intent cargar;
+    EditText txtCve, txtNombre, txtPrecio;
+
+    ProgressDialog progreso;
+    RequestQueue request;
+    JsonArrayRequest JsonArrayRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_servicio);
-
         inicializaComponentes();
-
+        request= Volley.newRequestQueue(this);
         btnRegresar.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -56,6 +72,20 @@ public class registro_servicio extends AppCompatActivity {
     }
 
     private void cargarWebService() {
+        if(txtCve.getText().toString().equals("")||
+                txtNombre.getText().toString().equals("")||
+                txtPrecio.getText().toString().equals("")){
+            Toast.makeText(this,"Todos los campos deben de estan correctamente llenados",Toast.LENGTH_SHORT);
+        }else{
+            progreso= new ProgressDialog(this);
+            progreso.setMessage("Cargando...");
+            progreso.show();
+            String url ="https://veterinary-clinic-ws.herokuapp.com/servicios/create/"+txtCve.getText().toString()+"" +
+                    "/"+txtNombre.getText().toString()+"/"+txtPrecio.getText().toString()+"/";
+            url= url.replace(" ", "%20");
+            JsonArrayRequest= new JsonArrayRequest(Request.Method.GET,url,null,this,this);
+            request.add(JsonArrayRequest);
+        }
     }
 
     private void inicializaComponentes() {
@@ -63,6 +93,9 @@ public class registro_servicio extends AppCompatActivity {
         btnHome=(ImageButton) findViewById(R.id.activityRegistroServicioBtnHome);
         btnGuardar=(Button) findViewById(R.id.activity_registro_servicio_btnGuardar);
         btnCancelar=(Button) findViewById(R.id.activity_registro_servicio_btnCancelar);
+        txtCve=(EditText) findViewById(R.id.activity_registro_servicio_txtCve);
+        txtPrecio=(EditText) findViewById(R.id.activity_registro_servicio_txtPrecio);
+        txtNombre=(EditText) findViewById(R.id.activity_registro_servicio_txtNombre);
     }
     private void cargaHome() {
         cargar = new Intent(this, Principal.class);
@@ -72,5 +105,24 @@ public class registro_servicio extends AppCompatActivity {
     private void cargaMascotas() {
         cargar = new Intent(this, Servicios.class);
         this.startActivity(cargar);
+    }
+
+
+    //======Métodos de Volley
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        progreso.hide();
+        Toast.makeText(this,"No se pudo registrar"+error.toString(),Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onResponse(JSONArray response) {
+        progreso.hide();
+        Toast.makeText(this,"Registro agregado exitosamente",Toast.LENGTH_SHORT).show();
+        txtNombre.setText("");
+        txtCve.setText("");
+        txtPrecio.setText("");
+        txtNombre.setText("");
     }
 }
