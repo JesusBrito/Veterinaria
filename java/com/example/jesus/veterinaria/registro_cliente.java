43aa26bfe23be12e62007130538af08a9567326e
JesusBrito <jesus291196@gmail.com>
2017-12-10 04:51:28 -0600

    Registro de las mascotas en la BD finalizado. Bitacora personal: Tengo sueño Unu

--- java/com/example/jesus/veterinaria/registro_cliente.java
+++ java/com/example/jesus/veterinaria/registro_cliente.java
@@ -2,8 +2,8 @@ package com.example.jesus.veterinaria;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,11 +15,9 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 public class registro_cliente extends AppCompatActivity implements Response.Listener<JSONArray>, Response.ErrorListener {
     Button btnGuardar, btnCancelar;
@@ -74,16 +72,22 @@ public class registro_cliente extends AppCompatActivity implements Response.List
 
     private void cargarWebService() {
         //INICIAR WEB SERVICE
-        progreso= new ProgressDialog(this);
-        progreso.setMessage("Cargando...");
-        progreso.show();
-        String url ="https://veterinary-clinic-ws.herokuapp.com/clientes/create/"+txtRFC.getText().toString()+
-                "/"+txtNom.getText().toString()+" "+txtApe.getText().toString()+"/"+txtDirec.getText().toString()+
-                "/"+txtTel.getText().toString()+"/"+txtEmail.getText().toString()+"/";
+        if(txtRFC.getText().toString().equals("")||txtNom.getText().toString().equals("")||txtApe.getText().toString().equals("")||
+                txtDirec.getText().toString().equals("")||txtTel.getText().toString().equals("")||
+                txtEmail.getText().toString().equals("")){
+            Toast.makeText(this,"Todos los campos deben de estan correctamente llenados",Toast.LENGTH_SHORT);
+        }else{
+            progreso= new ProgressDialog(this);
+            progreso.setMessage("Cargando...");
+            progreso.show();
+            String url ="https://veterinary-clinic-ws.herokuapp.com/clientes/create/"+txtRFC.getText().toString()+
+                    "/"+txtNom.getText().toString()+" "+txtApe.getText().toString()+"/"+txtDirec.getText().toString()+
+                    "/"+txtTel.getText().toString()+"/"+txtEmail.getText().toString()+"/";
 
-        url= url.replace(" ", "%20");
-        JsonArrayRequest= new JsonArrayRequest(Request.Method.GET,url,null,this,this);
-        request.add(JsonArrayRequest);
+            url= url.replace(" ", "%20");
+            JsonArrayRequest= new JsonArrayRequest(Request.Method.GET,url,null,this,this);
+            request.add(JsonArrayRequest);
+        }
     }
 
     private void inicializaComponentes() {
--- java/com/example/jesus/veterinaria/registro_mascota.java
+++ java/com/example/jesus/veterinaria/registro_mascota.java
@@ -1,20 +1,39 @@
 package com.example.jesus.veterinaria;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class registro_mascota extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
+public class registro_mascota extends AppCompatActivity implements Response.Listener<JSONArray>, Response.ErrorListener {
     Button btnGuardar, btnCancelar;
     ImageButton btnRegresar, btnHome;
+    EditText txtNombre, txtEspecie, txtRaza, txtRfcCliente, txtColor, txtTamaño, txtSenia, txtNacimiento;
+    String cveMascota="";
     Intent cargar;
+
+    ProgressDialog progreso;
+    RequestQueue request;
+    JsonArrayRequest JsonArrayRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_mascota);
+        request= Volley.newRequestQueue(this);
         inicializaComponentes();
 
         btnRegresar.setOnClickListener(
@@ -55,6 +74,38 @@ public class registro_mascota extends AppCompatActivity {
     }
 
     private void cargarWebService() {
+        if(txtEspecie.getText().toString().equals("")||
+                txtNombre.getText().toString().equals("")||
+                txtRaza.getText().toString().equals("")||
+                txtRfcCliente.getText().toString().equals("")||
+                txtColor.getText().toString().equals("")||
+                txtTamaño.getText().toString().equals("")||
+                txtSenia.getText().toString().equals("")||
+                txtNacimiento.getText().toString().equals("")){
+                Toast.makeText(this,"Todos los campos deben de estan correctamente llenados",Toast.LENGTH_SHORT);
+        }else{
+            progreso= new ProgressDialog(this);
+            progreso.setMessage("Cargando...");
+            progreso.show();
+            String rfcCliente=txtRfcCliente.getText().toString();
+            String nombreMascota=txtNombre.getText().toString();
+            for (int i=0; i<3;i++){
+                cveMascota+=nombreMascota.charAt(i);
+            }
+            cveMascota+="_";
+            for (int i=0; i<3;i++){
+                cveMascota+=rfcCliente.charAt(i);
+            }
+
+            String url ="https://veterinary-clinic-ws.herokuapp.com/mascotas/create/"+cveMascota+"/"+rfcCliente+
+                    "/"+nombreMascota+"/"+txtEspecie.getText().toString()+"/"+txtRaza.getText().toString()+
+                    "/"+txtColor.getText().toString()+"/"+txtTamaño.getText().toString()+"/"+txtSenia.getText().toString()+
+                    "/"+txtNacimiento.getText().toString()+"";
+
+            url= url.replace(" ", "%20");
+            JsonArrayRequest= new JsonArrayRequest(Request.Method.GET,url,null,this,this);
+            request.add(JsonArrayRequest);
+        }
     }
 
     private void inicializaComponentes() {
@@ -62,6 +113,14 @@ public class registro_mascota extends AppCompatActivity {
         btnHome=(ImageButton) findViewById(R.id.activityRegistroMascotasBtnHome);
         btnGuardar=(Button) findViewById(R.id.activity_registro_mascota_btnGuardar);
         btnCancelar=(Button) findViewById(R.id.activity_registro_mascota_btnCancelar);
+        txtNombre=(EditText) findViewById(R.id.activity_registro_mascota_txtNom);
+        txtColor=(EditText) findViewById(R.id.activity_registro_mascota_txtcolor);
+        txtEspecie=(EditText) findViewById(R.id.activity_registro_mascota_txtespecie);
+        txtRaza=(EditText) findViewById(R.id.activity_registro_mascota_txtraza);
+        txtRfcCliente=(EditText) findViewById(R.id.activity_registro_mascota_txtRfcCliente);
+        txtTamaño=(EditText) findViewById(R.id.activity_registro_mascota_txtTamanio);
+        txtSenia=(EditText) findViewById(R.id.activity_registro_mascota_txtsena);
+        txtNacimiento=(EditText) findViewById(R.id.activity_registro_mascota_txtNacimiento);
     }
     private void cargaHome() {
         cargar = new Intent(this, Principal.class);
@@ -71,5 +130,27 @@ public class registro_mascota extends AppCompatActivity {
     private void cargaMascotas() {
         cargar = new Intent(this, Mascotas.class);
         this.startActivity(cargar);
+    }
+
+    //======Métodos de Volley
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        progreso.hide();
+        Toast.makeText(this,"No se pudo registrar"+error.toString(),Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onResponse(JSONArray response) {
+        progreso.hide();
+        Toast.makeText(this,"Registro agregado exitosamente",Toast.LENGTH_SHORT).show();
+        txtNombre.setText("");
+        txtColor.setText("");
+        txtNacimiento.setText("");
+        txtSenia.setText("");
+        txtRaza.setText("");
+        txtEspecie.setText("");
+        txtTamaño.setText("");
+        txtRfcCliente.setText("");
     }
 }
